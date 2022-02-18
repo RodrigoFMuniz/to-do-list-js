5767b97156554694c647f60e212c38b1400517d0
RodrigoFMuniz <rodrigo.muniz.dev@gmail.com>
2022-02-17 23:54:34 -0300

    Init

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>Ok</div>
+    <div>Hello World</div>
   </React.StrictMode>,
   document.getElementById('root')
 );
